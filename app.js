c9df320a96d8eb1d525dd04e34fdf0228e8d0270
Vernon Lillies <vernon.lillies@gmail.com>
2018-01-04 20:33:38 -0500

    feat: add popup to show mural data on click

--- app.js
+++ app.js
@@ -5,7 +5,9 @@
                         {name: 'subways', path: './data/stm.geojson', geojson: ""},
                         {name: 'murals', path: './data/murals.geojson', geojson: ""}]
 
-  const loadGeojsonFilesAndRunMain = () => {
+  loadGeojsonFilesAndRunMain()
+
+  function loadGeojsonFilesAndRunMain () {
     let q = d3.queue()
     for (let file of geojsonFiles){
       q.defer(d3.json, file.path)
@@ -20,20 +22,11 @@
     })
   }
 
-  loadGeojsonFilesAndRunMain()
-
   function addFilesToGeojsonFiles(files) {
     geojsonFiles.map((file, i) => file.geojson = files[i])
   }
 
   function main () {
-
-    d3.selection.prototype.moveToFront = function() {
-      return this.each(function(){
-        this.parentNode.appendChild(this);
-      });
-    };
-
     const svgWidth = 960
     const svgHeight = 600
 
@@ -89,15 +82,25 @@
       .append('path')
       .attr('d', path)
       .classed('mural', true)
-      .on('mouseover', function(){
-        d3.select(this)
-          .moveToFront()
-          .classed('active', true)
-      })
-      .on('mousemove', handleMouseOverMural)
+      .on('mouseover', handleMouseOverMural)
+      .on('mousemove', handleMouseMoveMural)
       .on('mouseout', handleMouseOutMural)
+      .on('mousedown', handleMouseClickMural)
 
-    function handleMouseOverMural (d) {
+    function handleMouseOverMural(){
+      d3.select(this)
+        .moveToFront()
+        .classed('active', true)
+    }
+
+    //create a method used to bring the active svg to front
+    d3.selection.prototype.moveToFront = function() {
+      return this.each(function(){
+        this.parentNode.appendChild(this);
+      });
+    };
+
+    function handleMouseMoveMural (d) {
       tooltip
         .classed('active', true)
         .style('left', d3.event.x - (tooltip.node().offsetWidth /2 ) + 'px')
@@ -111,6 +114,28 @@
       tooltip
         .classed('active', false)
     }
-  }
 
+    function handleMouseClickMural (d) {
+      d3.select('.container')
+        .append('div')
+        .classed('popup', true)
+          .html(
+            `<a id='back' class="btn btn-primary">BACK TO MAP</a>
+             <img src=${d.properties.image}>
+                <div>
+                  <h4 style='margin-top:5px'>Created by: ${d.properties.artiste} in ${d.properties.annee}</h4>
+                  <h4>Located at: ${d.properties.adresse}</h4>
+                </div>
+              </div>`
+          )
+
+      d3.select('#back')
+        .on('click', handleBackButton)
+    }
+
+    function handleBackButton () {
+      d3.select('.popup')
+        .remove()
+    }
+  }
 })()
