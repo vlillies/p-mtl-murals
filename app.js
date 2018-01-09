255239524e13c5f273e1a4f395a0fab2900e2f03
Vernon Lillies <vernon.lillies@gmail.com>
2018-01-09 16:57:55 -0500

    feat: add modale on mural selection

--- app.js
+++ app.js
@@ -16,13 +16,13 @@
       if (err) {
         throw err
       } else {
-        addFilesToGeojsonFiles(files)
+        loadGeojsonFiles(files)
         main()
       }
     })
   }
 
-  function addFilesToGeojsonFiles(files) {
+  function loadGeojsonFiles(files) {
     geojsonFiles.map((file, i) => file.geojson = files[i])
   }
 
@@ -31,25 +31,34 @@
     const svgWidth = 960
     const svgHeight = 600
 
-    const projection = d3.geoMercator()
-      .rotate([0, -30, 0]).fitSize([svgWidth, svgHeight], geojsonFiles[0].geojson)
-
-    const path = d3.geoPath()
-      .projection(projection)
-
     const subwayColorScale = d3.scaleOrdinal()
-      .domain(['bleue', 'jaune', 'orange', 'verte'])
-      .range(['blue', 'yellow', 'orange', 'green'])
+        .domain(['bleue', 'jaune', 'orange', 'verte'])
+        .range(['blue', 'yellow', 'orange', 'green'])
 
     const muralColorScale = d3.scaleOrdinal()
-      .domain([1, 174])
-      .range(d3.schemeCategory20)
+        .domain([1, 174])
+        .range(d3.schemeCategory20)
+
+    const tooltip = d3.select('body')
+        .append('div')
+        .classed('tooltip', true)
+
+    const mask = d3.select('body')
+        .append('div')
+        .classed('mask', true)
+        .on('click tap', handleBackButton)
+
+    const popup = d3.select('.container')
+        .append('div')
+        .classed('popup', true)
 
     const svg = d3.select('svg')
 
-    const tooltip = d3.select('body')
-      .append('div')
-      .classed('tooltip', true)
+    const projection = d3.geoMercator()
+        .rotate([0, -30, 0]).fitSize([svgWidth, svgHeight], geojsonFiles[0].geojson)
+
+    const path = d3.geoPath()
+        .projection(projection)
 
     svg
       .attr('width', svgWidth)
@@ -91,7 +100,7 @@
       .attr('d', path)
       .classed('neighbourhood', true)
 
-    svg
+      svg
       .selectAll('.subway')
       .data(geojsonFiles[2].geojson.features)
       .enter()
@@ -142,26 +151,34 @@
     }
 
     function handleMouseClickMural (d) {
-      d3.select('.container')
-        .append('div')
-        .classed('popup', true)
-          .html(
-            `<a id='back' class="btn btn-primary">BACK TO MAP</a>
-             <img src=${d.properties.image}>
-                <div>
-                  <h4 style='margin-top:5px'>Created by: ${d.properties.artiste} in ${d.properties.annee}</h4>
-                  <h4>Located at: ${d.properties.adresse}</h4>
-                </div>
-              </div>`
-          )
+      mask
+        .classed('active', true)
+
+      popup
+        .classed('active', true)
+        .html(
+          `<a id='back' class="btn btn-primary">BACK TO MAP</a>
+           <img src=${d.properties.image}>
+              <div>
+                <h4 style='margin-top:5px'>Created by: ${d.properties.artiste} in ${d.properties.annee}</h4>
+                <h4>Located at: ${d.properties.adresse}</h4>
+              </div>
+            </div>`
+        )
 
       d3.select('#back')
         .on('click tap', handleBackButton)
+
     }
 
     function handleBackButton () {
-      d3.select('.popup')
-        .remove()
+      popup
+        .classed('active', false)
+
+      mask
+        .classed('active', false)
     }
+
+    console.log(mask)
   }
 })()
